3cb8133763e285208dda20b6ddd8c416b49e875a
inkodeapp <53095543+inkodeapp@users.noreply.github.com>
2021-02-03 17:44:38 -0300

    practica rocketcode

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-'use strict'
+"use strict";
+
+// variables que no voy a modificar
+const namePlayer = "Johan";
+const buttonYellow = "Yellow";
+const buttonGreen = "Green";
+const buttonRed = "Red";
+const buttonBlue = "Blue";
+
+//variables dinamicas
+
+let levelSequence = 1;
+let levelUser = 0;
+
+alert("bienvenido " + prompt("dime tu nombre", ""));
 
 
-function showMessage(a="sin texto",b){
-    alert(a+" "+b);
-}
 
-showMessage("hola")
+
+
